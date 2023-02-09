c855a35ccdb710c22793238b067576d7e137a120
Progressm1 <122403364+Progressm1@users.noreply.github.com>
2023-02-09 23:42:23 +0530

    Create alphabeta.py

---
+++ alphabeta.py
@@ -0,0 +1,31 @@
+import math
+
+MIN, MAX = -1000, 1000
+
+def alphabeta(depth, idx, maximizing, vals, alpha, beta):
+    if depth == int(math.log(len(vals), 2)): # checking if leaf node
+        return vals[idx]
+
+    if maximizing:
+        best = MIN
+        for i in range(2):
+            val = alphabeta(depth + 1, idx * 2 + i, False, vals, alpha, beta)
+            best = max(best, val)
+            if best >= beta:
+                return best
+            alpha = max(alpha, best)
+        return best
+    else:
+        best = MAX
+        for i in range(2):
+            val = alphabeta(depth + 1, idx * 2 + i, True, vals, alpha, beta)
+            best = min(best, val)
+            if best <= alpha:
+                return best
+            beta = min(beta, best)
+        return best
+
+if __name__ == "__main__":
+    values = [2,3,5,9,0,1,7,5]
+    result = alphabeta(0, 0, True, values, MIN, MAX)
+    print("Result = {}".format(result))
